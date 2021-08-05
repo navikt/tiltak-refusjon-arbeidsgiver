26af7c80555b205cd7c781a03e7fe46b9b68a18c
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-05 22:58:49 +0200

    lagt til sentry integrasjon i applikasjonen

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Sentry from '@sentry/browser';
+
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: 'https://6e57e48b384e45d797d8278d9e963916@sentry.gc.nav.no/87',
+        release: process.env.GIT_COMMIT_HASH || 'unknown',
+        environment: window.location.hostname,
+        integrations: [new Sentry.Integrations.Breadcrumbs()],
+    });
+}
 
 ReactDOM.render(
     <React.StrictMode>
