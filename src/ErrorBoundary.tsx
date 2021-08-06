ed660d0820d4ecf1fb0dbb6b57b01aefae969eec
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-06 13:12:29 +0200

    fikset importfeil i errorboundary
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,7 +1,5 @@
 import React, { ReactNode } from 'react';
 import * as Sentry from '@sentry/browser';
-import { Simulate } from 'react-dom/test-utils';
-import error = Simulate.error;
 
 // Denne er kopiert fra: https://reactjs.org/docs/concurrent-mode-suspense.html#handling-errors
 class ErrorBoundary extends React.Component<{ fallback: ReactNode }> {
@@ -14,7 +12,7 @@ class ErrorBoundary extends React.Component<{ fallback: ReactNode }> {
     }
     render() {
         if (this.state.hasError) {
-            Sentry.captureException(error);
+            Sentry.captureException(this.state.error);
             return this.props.fallback;
         }
         return this.props.children;
