fbc7dbc315502a6b02d08d6444252004a97a8a02
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-12 15:10:02 +0200

    oppdatert ACL origin i cors settings

--- server/src/cors.js
+++ server/src/cors.js
@@ -1,5 +1,5 @@
 export default (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.HOST);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, X-Requested-With');
     return next();
