06b54d964bd31c8b3266d62ccaa17ff0d8b32163
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-12 11:16:31 +0200

    oppdatert cors

--- server/src/cors.js
+++ server/src/cors.js
@@ -5,7 +5,7 @@ export default (req, res, next) => {
     console.log('origin:', origin);
 
     if (navDomain) {
-        res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, X-Requested-With, Authorization');
