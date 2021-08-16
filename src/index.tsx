360bf6f704544fef96f1dd988a23732e3c0ad925
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-16 16:30:13 +0200

    Nytt sentry init oppsett
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { init as Sentry, Integrations, captureException, configureScope } from '@sentry/react';
+import * as Sentry from '@sentry/browser';
 
 // sentry init
-Sentry({
-    dsn: 'https://6e57e48b384e45d797d8278d9e963916@sentry.gc.nav.no/87',
-    release: process.env.GIT_COMMIT_HASH || 'unknown',
-    environment: window.location.hostname,
-    integrations: [new Integrations.Breadcrumbs({ console: false })],
-    autoSessionTracking: false,
-});
-configureScope((scope) => {
-    scope.setTag('commit', process.env.GIT_COMMIT_HASH);
-});
+Sentry.init({ dsn: 'https://6e57e48b384e45d797d8278d9e963916@sentry.gc.nav.no/87' });
 
 ReactDOM.render(
     <React.StrictMode>
@@ -23,10 +14,6 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-export const sendSentryException = (message: Error | null) => {
-    captureException(message);
-};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
