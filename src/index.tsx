a8b706b1d307e5f0331450e732bc62daf23e9eb6
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-16 10:42:13 +0200

    oppdatert sentry init
    Co-authored-by: Sindre Dahl Løken <sindre.loken@nav.no>

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/browser';
 
 // sentry init
-Sentry.init({
-    dsn: 'https://6e57e48b384e45d797d8278d9e963916@sentry.gc.nav.no/87',
-    release: process.env.GIT_COMMIT_HASH ?? 'unknown',
-    environment: window.location.hostname,
-    integrations: [new Sentry.Integrations.Breadcrumbs()],
-});
+Sentry.init({ dsn: 'https://6e57e48b384e45d797d8278d9e963916@sentry.gc.nav.no/87' });
 
 ReactDOM.render(
     <React.StrictMode>
