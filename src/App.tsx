e03e6be360e052d81154442bd0cda75408d68665
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-17 12:42:22 +0200

    Lagt til redirect i App via react router om appen mottar header med status 301.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { FunctionComponent, useEffect } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AdvarselBannerTestversjon from './AdvarselBannerTestversjon/AdvarselBannerTestversjon';
 import './App.css';
 import { BrukerProvider } from './bruker/BrukerContext';
@@ -11,10 +11,34 @@ import OversiktSide from './refusjon/OversiktSide/OversiktSide';
 import Refusjon from './refusjon/RefusjonSide/Refusjon';
 import { registrereBesok } from './utils/amplitude-utils';
 
+interface Props {
+    from: string;
+    to: string;
+    status: number;
+    children: React.ReactNode;
+}
+
 function App() {
     useEffect(() => {
         registrereBesok();
     });
+
+    const RedirectWithStatus: FunctionComponent<Props> = (props: Props) => {
+        const { from, to, status, children } = props;
+        return (
+            <Route
+                render={({ staticContext }) => {
+                    if (staticContext) staticContext.statusCode = status;
+                    return (
+                        <Redirect from={from} to={to}>
+                            {children}
+                        </Redirect>
+                    );
+                }}
+            />
+        );
+    };
+
     return (
         <BrowserRouter>
             <ScrollToTop />
@@ -24,18 +48,20 @@ function App() {
                     <Landingsside />
                 </Route>
                 <BrukerProvider>
-                    <div style={{ minHeight: '10rem', padding: '0.5rem' }}>
-                        <Route exact path="/refusjon">
-                            <ErrorOgSuspenseHandler>
-                                <OversiktSide />
-                            </ErrorOgSuspenseHandler>
-                        </Route>
-                        <Route path="/refusjon/:refusjonId">
-                            <ErrorOgSuspenseHandler>
-                                <Refusjon />
-                            </ErrorOgSuspenseHandler>
-                        </Route>
-                    </div>
+                    <RedirectWithStatus from="/refusjon" to="/login" status={301}>
+                        <div style={{ minHeight: '10rem', padding: '0.5rem' }}>
+                            <Route exact path="/refusjon">
+                                <ErrorOgSuspenseHandler>
+                                    <OversiktSide />
+                                </ErrorOgSuspenseHandler>
+                            </Route>
+                            <Route path="/refusjon/:refusjonId">
+                                <ErrorOgSuspenseHandler>
+                                    <Refusjon />
+                                </ErrorOgSuspenseHandler>
+                            </Route>
+                        </div>
+                    </RedirectWithStatus>
                 </BrukerProvider>
             </Switch>
         </BrowserRouter>
