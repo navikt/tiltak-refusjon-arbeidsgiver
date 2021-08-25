bebfb11c3834449446ed3bd642035b6ed6692cb5
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-08-25 20:32:22 +0200

    fjernet unused vars

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { kontaktLogin } from './services/rest-service';
 
 const Login: FunctionComponent = () => {
