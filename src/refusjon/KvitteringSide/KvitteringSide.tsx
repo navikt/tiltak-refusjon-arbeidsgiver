42808b27560d9a0587f18f4e44b28ebf6a98a28b
Mats Bolstad <mbolstad@gmail.com>
2021-09-07 13:13:22 +0200

    Flytter svar på inntektspørsmål til egen komponent, for å gjøre det likt som saksbehandler-app.

--- src/refusjon/KvitteringSide/KvitteringSide.tsx
+++ src/refusjon/KvitteringSide/KvitteringSide.tsx
@@ -10,6 +10,7 @@ import Utregning from '../../komponenter/Utregning';
 import InntekterFraAMeldingen from '../RefusjonSide/InntekterFraAMeldingen';
 import { formatterPenger } from '../../utils/PengeUtils';
 import { tiltakstypeTekst } from '../../messages';
+import InntekterFraTiltaketSvar from '../RefusjonSide/InntekterFraTiltaketSvar';
 
 const KvitteringSide: FunctionComponent = () => {
     const { refusjonId } = useParams();
@@ -31,22 +32,7 @@ const KvitteringSide: FunctionComponent = () => {
             <VerticalSpacer rem={2} />
             <InntekterFraAMeldingen />
             <VerticalSpacer rem={2} />
-            {refusjon.inntekterKunFraTiltaket !== null && refusjon.inntekterKunFraTiltaket !== undefined && (
-                <div>
-                    <Element>
-                        Er inntektene som vi har hentet ({formatterPenger(refusjon.inntektsgrunnlag.bruttoLønn)}) kun
-                        fra tiltaket {tiltakstypeTekst[refusjon.tilskuddsgrunnlag.tiltakstype]}?{' '}
-                    </Element>
-                    <Normaltekst>{refusjon.inntekterKunFraTiltaket ? 'Ja' : 'Nei'}</Normaltekst>
-                    {refusjon.korrigertBruttoLønn !== null && refusjon.korrigertBruttoLønn !== undefined && (
-                        <>
-                            <VerticalSpacer rem={1} />
-                            <Element>Korrigert brutto lønn:</Element>
-                            <Normaltekst>{formatterPenger(refusjon.korrigertBruttoLønn)}</Normaltekst>
-                        </>
-                    )}
-                </div>
-            )}
+            <InntekterFraTiltaketSvar />
             <VerticalSpacer rem={2} />
             <Utregning beregning={refusjon.beregning} tilskuddsgrunnlag={refusjon.tilskuddsgrunnlag} />
             <VerticalSpacer rem={4} />
