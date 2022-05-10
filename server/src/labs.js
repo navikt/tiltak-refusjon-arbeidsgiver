456a5a747b8c8ba59c3e752c9c93b7bb71bc910f
havardmj <havard.myrvold.johannessen@nav.no>
2022-05-10 17:20:32 +0200

    oppdatert proxy config labs

--- server/src/labs.js
+++ server/src/labs.js
@@ -35,10 +35,20 @@ async function startLabs(server) {
 
         server.use(express.static(path.join(__dirname, '../build')));
 
+        const restream = (proxyReq, req, res, options) => {
+            if (req.body) {
+                let bodyData = JSON.stringify(req.body);
+                proxyReq.setHeader('Content-Type', 'application/json');
+                proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+                proxyReq.write(bodyData);
+            }
+        };
+
         server.use(
             '/api',
             createProxyMiddleware({
                 target: 'http://tiltak-refusjon-api',
+                onProxyReq: restream,
                 changeOrigin: true,
                 proxyTimeout: 30000,
                 secure: true,
