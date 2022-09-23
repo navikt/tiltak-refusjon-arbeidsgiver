7e4d3a56bbe7f854a0eb071f5e5d914e95b155f6
Sindre Dahl Løken <sindre.loken@nav.no>
2022-09-23 10:23:38 +0200

    bruker funksjon for generere navn på mned

--- src/refusjon/RefusjonSide/inntektsmelding/InntektsMeldingHeader.tsx
+++ src/refusjon/RefusjonSide/inntektsmelding/InntektsMeldingHeader.tsx
@@ -1,8 +1,7 @@
-import moment from 'moment';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import { FunctionComponent } from 'react';
 import BEMHelper from '../../../utils/bem';
-import { formatterDato, NORSK_DATO_OG_TID_FORMAT } from '../../../utils/datoUtils';
+import { formatterDato, månedsNavn, NORSK_DATO_OG_TID_FORMAT } from '../../../utils/datoUtils';
 import { Refusjon } from '../../refusjon';
 
 interface Properties {
@@ -11,7 +10,7 @@ interface Properties {
 
 const InntektsMeldingHeader: FunctionComponent<Properties> = ({ refusjon }: Properties) => {
     const cls = BEMHelper('inntektsmelding');
-    const månedNavn = moment(refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddFom).format('MMMM');
+    const månedNavn = månedsNavn(refusjon.refusjonsgrunnlag.tilskuddsgrunnlag.tilskuddFom);
 
     return (
         <div className={cls.element('header')}>
