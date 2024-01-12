bf25e3e1c386cc493a9e055209e24d0bb387a32a
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-01-12 14:30:32 +0100

    Litt proxxy-fiks

--- vite.config.ts
+++ vite.config.ts
@@ -20,32 +20,62 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': { target: 'http://localhost:8081', changeOrigin: true },
+            '/dekoratoren/env': {
+                target: 'http://localhost:3001/',
+                bypass(req, res, options) {
+                    axios
+                        .get(
+                            'https://www.nav.no/dekoratoren/env?context=arbeidsgiver&feedback=false&level=Level4&redirectToApp=true'
+                        )
+                        .then(
+                            (response) => {
+                                res.end(
+                                    JSON.stringify({
+                                        ...response.data,
+                                        API_INNLOGGINGSLINJE_URL: '/dekoratoren/api',
+                                        APP_URL: '/dekoratoren',
+                                        LOGOUT_URL: '/logout',
+                                    })
+                                );
+                            },
+                            (error) => {
+                                console.log('Feil i dekorator-env', error);
+                                res.end(JSON.stringify({}));
+                            }
+                        );
+                },
+            },
             '/dekoratoren/api/auth': {
-                target: 'http://localhost:8081',
+                target: 'http://localhost:3001',
                 bypass(req, res, options) {
                     axios
                         .get('http://localhost:8081/api/arbeidsgiver/innlogget-bruker', {
                             headers: req.headers,
+                            secure: false,
                         })
                         .then(
                             (response) => {
                                 res.end(JSON.stringify({ ...response.data, ident: response.data.identifikator || '' }));
                             },
                             (error) => {
+                                console.error('Feil i dekorator-auth', error);
                                 res.end(JSON.stringify({ authenticated: false }));
                             }
                         );
                 },
             },
-            '/internarbeidsflatedecorator': {
-                target: 'https://navikt.github.io/',
-                changeOrigin: true,
-                rewrite(path) {
-                    return path.replace(
-                        '/internarbeidsflatedecorator',
-                        'https://navikt.github.io/internarbeidsflatedecorator'
-                    );
+            '/logout': {
+                target: 'http://localhost:3001/',
+                bypass(req, res, options) {
+                    res.setHeader('set-cookie', 'tokenx-token=; max-age=0');
+                    res.setHeader('set-cookie', 'aad-token=; max-age=0');
+                    res.writeHead(302, { Location: '/' });
+                    res.end();
                 },
+            },
+            '/dekoratoren': {
+                target: 'https://www.nav.no',
+                changeOrigin: true,
             },
         },
     },
