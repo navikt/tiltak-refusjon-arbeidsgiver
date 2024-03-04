d02e65ffdf279ea3fbaae1eddff678713413e0d5
Millad Dagdoni <milladd@gmail.com>
2024-03-04 17:48:27 +0100

    Lagt til mulighet for å skrive hva brukeren vil i brutto lønn feltet. Det kommer valideringer underveis. Verdien blir 0 i utregning om det er ugyldig. Feilmeldingen går ikke bort helt til bruker har tastet riktig verdig.

--- src/refusjon/RefusjonSide/BruttolønnUtbetaltInput.tsx
+++ src/refusjon/RefusjonSide/BruttolønnUtbetaltInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FunctionComponent, PropsWithChildren, SetStateAction } from 'react';
+import React, { ChangeEvent, FunctionComponent, PropsWithChildren, SetStateAction, useState } from 'react';
 import { BodyShort, TextField } from '@navikt/ds-react';
 import { Inntektsgrunnlag, Refusjon } from '../refusjon';
 import { tiltakstypeTekst } from '@/messages';
@@ -23,25 +23,53 @@ const BruttolønnUtbetaltInput: FunctionComponent<Properties> = ({
     const cls = BEMHelper('refusjonside');
     const sumInntekterOpptjent: number = sumInntekterOpptjentIPeriode(inntektsgrunnlag);
     const { tilskuddsgrunnlag, beregning } = refusjon.refusjonsgrunnlag;
+    const [lokalBruttolønnVerdi, setLokalBruttolønnVerdi] = useState('');
+    const [feilmelding, setFeilmelding] = useState('');
     return (
-        <TextField
-            className={cls.element('bruttolønn-utbetalt-for-periode')}
-            size="small"
-            label={`Skriv inn bruttolønn utbetalt for perioden med ${tiltakstypeTekst[tilskuddsgrunnlag.tiltakstype]}`}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                const verdi: string = event.currentTarget.value;
-                if (verdi.match(/^\d*$/) && parseInt(verdi, 10) <= sumInntekterOpptjent) {
+        <>
+            <TextField
+                className={cls.element('bruttolønn-utbetalt-for-periode')}
+                size="small"
+                label={`Skriv inn bruttolønn utbetalt for perioden med ${
+                    tiltakstypeTekst[tilskuddsgrunnlag.tiltakstype]
+                }`}
+                onChange={(event) => {
+                    const verdi: string = event.currentTarget.value;
+
+                    setLokalBruttolønnVerdi(verdi);
+
+                    if (verdi.trim().length > 0 && !verdi.match(/^\d*$/)) {
+                        setFeilmelding('Feltet må kun inneholde tall uten spesielle tegn eller symboler.');
+                        setEndringBruttoLønn('0');
+                        return;
+                    }
+                    if (verdi.trim().length > 0 && verdi.match(/^\d*$/) && parseInt(verdi, 10) > sumInntekterOpptjent) {
+                        setFeilmelding(
+                            `Tallet er høyre enn opptjent inntekt. Det må være det samme eller lavere enn ${sumInntekterOpptjent} kr.`
+                        );
+                        setEndringBruttoLønn('0');
+                        return;
+                    }
+
                     setEndringBruttoLønn(verdi);
-                }
-                if (!verdi) {
-                    setEndringBruttoLønn('');
-                }
-            }}
-            onBlur={() =>
-                delayEndreBruttolønn(refusjon.id!, false, refusjon.sistEndret, parseInt(endringBruttoLønn, 10))
-            }
-            value={endringBruttoLønn}
-        />
+                    setFeilmelding('');
+                    console.log('LAGRET ', verdi, 'CTXT: ', endringBruttoLønn);
+                }}
+                onBlur={(event) => {
+                    let verdi: string = event.currentTarget.value;
+                    if (feilmelding.trim().length !== 0) {
+                        verdi = '0';
+                        console.log('BLUR RESTE: ', endringBruttoLønn);
+                    }
+                    delayEndreBruttolønn(refusjon.id!, false, refusjon.sistEndret, parseInt(verdi, 10));
+
+                    setEndringBruttoLønn(verdi);
+                    console.log('BLUR ', verdi);
+                }}
+                value={lokalBruttolønnVerdi}
+            />
+            <div style={{ color: 'Red', fontWeight: 'Bold' }}>{feilmelding}</div>
+        </>
     );
 };
 export default BruttolønnUtbetaltInput;
